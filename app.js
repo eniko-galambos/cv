82cb437a615edcd2f441379e4954c9665615c906
Galambos Eniko <galaboseniko93@gmail.com>
2020-07-01 18:15:17 +0200

    Add page upper decor animation

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+let pageUpperDecor = document.querySelector(".page-upper-decor");
+window.addEventListener("scroll", function bodyScroll() {
+  let endPosition = 350;
+
+  if(document.body.scrollTop < endPosition && document.documentElement.scrollTop < endPosition) {
+    let position = document.documentElement.scrollTop || document.body.scrollTop;
+    let percent = position / endPosition;
+    pageUpperDecor.style.transform = `scaleY(${1 - percent})`;
+  } else {
+    pageUpperDecor.style.transform = `scaleY(0)`;
+  }
+});
+
 let handles = Array.from(document.querySelectorAll(".main-card-handle"));
 handles.forEach(function handlesForEach(handle) {
   handle.addEventListener("mouseover", function handleMouseEnter(event) {
